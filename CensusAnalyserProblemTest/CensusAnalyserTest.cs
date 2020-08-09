cd5b7a6029f760cd60e9d1b478373706cea956b5
seema1611 <seemarajpure16@gmail.com>
2020-08-09 22:57:01 +0700

    ADDED: testcase if path incorrect then throw FileNotFound exception

--- CensusAnalyserProblemTest/CensusAnalyserTest.cs
+++ CensusAnalyserProblemTest/CensusAnalyserTest.cs
@@ -12,17 +12,29 @@ namespace CensusAnalyserProblemTest
         string indianCensusDataHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
 
         string csvFilePath = @"C:\Users\User\source\repos\CensusAnalyserProblem\CensusAnalyserProblemTest\Resources\IndiaStateCensusData.csv";
+        string invalidCsvFilePath = @"D:\Users\User\source\repos\CensusAnalyserProblem\CensusAnalyserProblemTest\Resources\IndiaStateCensusData.csv";
+
         [SetUp]
         public void Setup()
         {
             censusAnalyser = new CensusAnalyser();
         }
 
+        //UC-1
+        //TC-1.1
         [Test]
         public void givenIndianCensusCSVFile_WhenFileExist_ShouldReturnsTotalNumberOfRecords()
         {
             IEnumerable<string> indianCensusRecord = censusAnalyser.loadCSVFileData(indianCensusDataHeaders,csvFilePath);
             Assert.AreEqual(29, indianCensusRecord.Count());
+        }
+
+        //TC-1.2
+        [Test]
+        public void givenIndianCensusCSVFile_WhenFileNotExist_ShouldThrowFileNotFoundException()
+        {
+            var error = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.loadCSVFileData(indianCensusDataHeaders, invalidCsvFilePath));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, error.type);
         }
     }
 }
