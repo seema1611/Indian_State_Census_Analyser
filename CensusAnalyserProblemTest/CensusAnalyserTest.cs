8611c27d221b0981cbaea0cc1b6d38541bb6ed8f
seema1611 <seemarajpure16@gmail.com>
2020-08-13 17:11:24 +0700

    [Seema]ADDED: testcase to get least populated state between INDIA and US

--- CensusAnalyserProblemTest/CensusAnalyserTest.cs
+++ CensusAnalyserProblemTest/CensusAnalyserTest.cs
@@ -344,7 +344,7 @@ namespace CensusAnalyserProblemTest
         //<--------------------UC-11-------------------->
         //TC-11.1
         [Test]
-        public void GivenUsCensusAndIndianCensusCSVFile_WhenFileExist_ShouldReturnsMostPopulousState()
+        public void GivenUsCensusAndIndianCensusCSVFile_Whensorted_ShouldReturnsMostPopulousState()
         {
             var usRecord = censusAdapter.LoadCensusData<USCensus>(Country.US, usCensusDataHeaders, usCSVFilePath);
             string usSortedList = censusAdapter.SortAndConvertCensusToJson(usRecord, DTO.US, SortBy.POPULATION_DENSITY, SortOrder.DESCENDING);
@@ -355,6 +355,21 @@ namespace CensusAnalyserProblemTest
             List<IndianCensus> indianCensusSortedList = JsonConvert.DeserializeObject<List<IndianCensus>>(sortedList);
 
             Assert.AreEqual("Bihar", censusAdapter.GetMostPopulousStateBetweenBoth(indianCensusSortedList[0], usCensusSortedList[0]));
+        }
+
+        //TC-11.2
+        [Test]
+        public void GivenUsCensusAndIndianCensusCSVFile_Whensorted_ShouldReturnsLeastPopulousState()
+        {
+            var usRecord = censusAdapter.LoadCensusData<USCensus>(Country.US, usCensusDataHeaders, usCSVFilePath);
+            string usSortedList = censusAdapter.SortAndConvertCensusToJson(usRecord, DTO.US, SortBy.POPULATION_DENSITY, SortOrder.DESCENDING);
+            List<USCensus> usCensusSortedList = JsonConvert.DeserializeObject<List<USCensus>>(usSortedList);
+
+            var indianStateRecord = censusAdapter.LoadCensusData<IndianCensus>(Country.INDIA, indianCensusDataHeaders, csvFilePath);
+            string sortedList = censusAdapter.SortAndConvertCensusToJson(indianStateRecord, DTO.INDIA_CENSUS, SortBy.POPULATION_DENSITY, SortOrder.DESCENDING);
+            List<IndianCensus> indianCensusSortedList = JsonConvert.DeserializeObject<List<IndianCensus>>(sortedList);
+
+            Assert.AreEqual("Arunachal Pradesh", censusAdapter.GetMostPopulousStateBetweenBoth(indianCensusSortedList[indianCensusSortedList.Count - 1], usCensusSortedList[0]));
         }
     }
 }
